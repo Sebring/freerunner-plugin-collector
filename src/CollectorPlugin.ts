36cf139fd99d4ea46d94be27fd473a174b010e18
J. G. Sebring <johan.sebring@gmail.com>
2021-10-13 22:22:16 +0200

    tidy: add prettier and editor settings

--- src/CollectorPlugin.ts
+++ src/CollectorPlugin.ts
@@ -4,11 +4,11 @@ export interface C_Collector extends NamedComponent {
 	collector(name: string, callback?: CollectorCallback): this
 }
 export interface E_Collector extends E_2D {
-    collector(name: string, callback: CollectorCallback): this
+	collector(name: string, callback: CollectorCallback): this
 }
 
 export interface E_Collectable extends E_2D {
-    collectable(name: string): this
+	collectable(name: string): this
 }
 
 export interface CollectableCallback {
@@ -19,15 +19,13 @@ export interface CollectorCallback {
 	(collector: E_Collectable): void
 }
 
-const CollectorPlugin = (function() {
-	function load(F: FGame) {
+const CollectorPlugin = {
+	name: 'CollectorPlugin',
+	load(F: FGame) {
 		F.createComponent(Collectable)
 		F.createComponent(Collector)
-	}
-	return { load, name: 'CollectorPlugin' }
-}());
-
-
+	},
+}
 
 export const Collectable: NamedComponent = {
 	name: 'Collectable',
@@ -43,23 +41,23 @@ export const Collectable: NamedComponent = {
 		return this
 	},
 	events: {
-		'$Collect': function(this: Component) {
+		$Collect: function (this: Component) {
 			if (!this._isCollected) {
 				this._isCollected = true
 			}
-		}
-	}
+		},
+	},
 }
 
 export const Collector: NamedComponent = {
-	name: "Collector",
+	name: 'Collector',
 	required: 'Collision, Motion',
 	init() {
 		this._collectorComp = []
 		return this
 	},
 	events: {
-		'Move': function(this: Component) {
+		Move: function (this: Component) {
 			for (const c of this._collectorComp) {
 				const hits = this.hit(c)
 				if (hits) {
@@ -67,16 +65,15 @@ export const Collector: NamedComponent = {
 						!hits[0].obj.trigger('$Collect', this)
 						this.trigger(`$Collected${c}`, hits[0].obj)
 					}
-				}				
+				}
 			}
-			
-		}
+		},
 	},
 	collector(this: Component, name: string, callback: CollectorCallback) {
-		this._collectorComp = this._collectorComp? [name, ...this._collectorComp] : [name]
+		this._collectorComp = this._collectorComp ? [name, ...this._collectorComp] : [name]
 		callback && this.bind(`$Collected${name}`, callback)
 		return this
-	}	
+	},
 }
 
 export default <FPlugin>CollectorPlugin
--- src/CollectorPluginDemo.ts
+++ src/CollectorPluginDemo.ts
@@ -2,108 +2,104 @@ import { Component, Entity, FGame, FPlugin, NamedComponent } from 'freerunner'
 import CollectorPlugin, { C_Collector, E_Collectable, E_Collector } from './CollectorPlugin'
 
 const CollectorPluginDemo: FPlugin = {
-    name: 'CollectorPluginDemo',
+	name: 'CollectorPluginDemo',
 	load(F: FGame) {
-        console.log('Running Collector demo')
-        
-        // load plugin
-        F.loadPlugin(CollectorPlugin)
-        
-        // init world
-        F.init(500, 500, 'game').background('powderblue')
+		console.log('Running Collector demo')
 
-        // add basic floor
-        F.createEntity('Floor, Color, DOM, 2D')
-            .attr({x:0, y:400, h:25, w:500})
-            .color('forestgreen')
-        F.createEntity('Floor, Color, DOM, 2D')
-            .attr({x:0, y:425, h:85, w:500})
-            .color('burlywood')
+		// load plugin
+		F.loadPlugin(CollectorPlugin)
 
-        /*
-            Most basic example by creating an entity and using the 
-            Collectable component directly, see how we must copy code to create more apples.
-        */
-        // add Apple - collectable: Apple
-        F.createEntity<E_Collectable>('Apple, Collectable, , Gravity, DOM , Color')
-            .attr({x:100, y:80, h:20, w:20})
-            .collectable('Apple')
-            .color('red')
-            .gravity('Floor')
-            .bind('$Collect', function(this: Entity, collector: C_Collector) {
-                console.log('Apple: Collected by', collector)
-                // delete entity
-                this.destroy()
-            })
+		// init world
+		F.init(500, 500, 'game').background('powderblue')
 
-        // add Player - collector: Apple
-        const player = F.createEntity<E_Collector>('Player, Gravity, Color, Twoway, DOM, Collector')
-            .attr({x: 200, y: 100, h:60, w:20 })
-            .twoway(200, 200).color('black')
-            .gravity('Floor')
-            .collector('Apple', function(collectable: E_Collectable) {
-                console.log('Player: Mmmm apple..', collectable)
-                /*
-                 Here is a good place to trigger cascading events related to player
-                 ex for inventory keeping, scores, stats
-                */
-            })
-            
+		// add basic floor
+		F.createEntity('Floor, Color, DOM, 2D').attr({ x: 0, y: 400, h: 25, w: 500 }).color('forestgreen')
+		F.createEntity('Floor, Color, DOM, 2D').attr({ x: 0, y: 425, h: 85, w: 500 }).color('burlywood')
 
-        /*
-        Another collectable example, a Hat
-        Instead of creating an entity directly we create a hat component, 
-        the result is the same, but this one is more generic, we could make 
-        entities with the hat component but giving them diffenet sizes, sprites etc
+		/*
+				Most basic example by creating an entity and using the 
+				Collectable component directly, see how we must copy code to create more apples.
+		  */
+		// add Apple - collectable: Apple
+		F.createEntity<E_Collectable>('Apple, Collectable, , Gravity, DOM , Color')
+			.attr({ x: 100, y: 80, h: 20, w: 20 })
+			.collectable('Apple')
+			.color('red')
+			.gravity('Floor')
+			.bind('$Collect', function (this: Entity, collector: C_Collector) {
+				console.log('Apple: Collected by', collector)
+				// delete entity
+				this.destroy()
+			})
 
-        We also create a HatCollector component - any entity with this component can 
-        pick up hats.
-        */
+		// add Player - collector: Apple
+		const player = F.createEntity<E_Collector>('Player, Gravity, Color, Twoway, DOM, Collector')
+			.attr({ x: 200, y: 100, h: 60, w: 20 })
+			.twoway(200, 200)
+			.color('black')
+			.gravity('Floor')
+			.collector('Apple', function (collectable: E_Collectable) {
+				console.log('Player: Mmmm apple..', collectable)
+				/*
+					  Here is a good place to trigger cascading events related to player
+					  ex for inventory keeping, scores, stats
+					 */
+			})
 
-        // Hat as collectable component
-        F.createComponent<NamedComponent>({
-            name: 'Hat',
-            required: 'Collectable, DOM, Color, 2D, Gravity',
-            init() {
-                this.color('olive')
-                this.gravity('Floor')
-                this.collectable('Hat')
-            },
-            events: {
-                '$Collect': function(this: Component, collector: C_Collector) {
-                    console.log('Hat: Collected by', collector)
-                    this.antigravity()
-                    collector.attach(this)
-                    this.y = collector.y - this.h
-                    this.x = collector.x - (this.w-collector.w)/2
-                }
-            }
-        })
+		/*
+		  Another collectable example, a Hat
+		  Instead of creating an entity directly we create a hat component, 
+		  the result is the same, but this one is more generic, we could make 
+		  entities with the hat component but giving them diffenet sizes, sprites etc
 
-        // HatCollector component
-        F.createComponent<NamedComponent>({
-            name: 'HatCollector',
-            required: 'Collector',
-            init: function() {
-                this.collector('Hat')
-            },
-            events: {
-                '$CollectedHat': function(collectable: E_Collectable) {
-                    console.log('HatCollector: Found a hat', collectable)
-                }
-            }
-        })
+		  We also create a HatCollector component - any entity with this component can 
+		  pick up hats.
+		  */
 
-        // create hat from component
-        F.createEntity('Hat').attr({x:350, y:300, h:10, w:60})
+		// Hat as collectable component
+		F.createComponent<NamedComponent>({
+			name: 'Hat',
+			required: 'Collectable, DOM, Color, 2D, Gravity',
+			init() {
+				this.color('olive')
+				this.gravity('Floor')
+				this.collectable('Hat')
+			},
+			events: {
+				$Collect: function (this: Component, collector: C_Collector) {
+					console.log('Hat: Collected by', collector)
+					this.antigravity()
+					collector.attach(this)
+					this.y = collector.y - this.h
+					this.x = collector.x - (this.w - collector.w) / 2
+				},
+			},
+		})
 
-        // add HatCollector to player 
-        // remove this line and player can't pick up the hat
-        player.addComponent('HatCollector')
+		// HatCollector component
+		F.createComponent<NamedComponent>({
+			name: 'HatCollector',
+			required: 'Collector',
+			init: function () {
+				this.collector('Hat')
+			},
+			events: {
+				$CollectedHat: function (collectable: E_Collectable) {
+					console.log('HatCollector: Found a hat', collectable)
+				},
+			},
+		})
 
-        // want another hat? See how easy it is with the component
-        // F.createEntity('Hat').attr({x:10, y:300, h:60, w:40}).color('goldenrod')
-    }
+		// create hat from component
+		F.createEntity('Hat').attr({ x: 350, y: 300, h: 10, w: 60 })
+
+		// add HatCollector to player
+		// remove this line and player can't pick up the hat
+		player.addComponent('HatCollector')
+
+		// want another hat? See how easy it is with the component
+		// F.createEntity('Hat').attr({x:10, y:300, h:60, w:40}).color('goldenrod')
+	},
 }
 
 export default CollectorPluginDemo
